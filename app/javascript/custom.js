69fb19c4c5e4e9ef0f1664c286071a937d0ba9c3
Connor Wright <connor.d.wright1996@gmail.com>
2020-05-04 00:56:10 +0000

    finished up navbar w/ bootstrap

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
     var $window     = $(window);
     var $burgernav  = $('#burger-nav');
+    var $navitem1   = $('#navbar-group-1');
     
     //function to perform actions when window gets resized
     function checkWidth() {
         var windowsize = $window.width();
         if(windowsize < 800){
+            $navitem1.removeClass('mr-auto');
             $burgernav.removeClass('d-none');
         }
         else{
+            $navitem1.addClass('mr-auto');
             $burgernav.addClass('d-none');
         }
     }
