6cb8202f65638946a84fe345e6b66e64cf2547f1
Connor Wright <connor.d.wright1996@gmail.com>
2020-05-10 23:10:28 +0000

    Got header margin working correctly

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,7 +1,16 @@
+/* global $*/
+import 'bootstrap';
 $(document).ready(function(){
+    displayBurgerNavOnResize();
+    adjustHeaderBufferOnBurgerNavClick();
+});
+
+function displayBurgerNavOnResize()
+{
     var $window     = $(window);
     var $burgernav  = $('#burger-nav');
     var $navitem1   = $('#navbar-group-1');
+    var $items      = $('#navbar-items');
     
     //function to perform actions when window gets resized
     function checkWidth() {
@@ -11,13 +20,34 @@ $(document).ready(function(){
             $burgernav.removeClass('d-none');
         }
         else{
+            
             $navitem1.addClass('mr-auto');
             $burgernav.addClass('d-none');
         }
+        
+        //always collapse navitems when a resize happens
+        $items.collapse('hide');
     }
     
     //execute on load
     checkWidth();
     //Bind to event
     $(window).resize(checkWidth);
-});
+}
+ 
+function adjustHeaderBufferOnBurgerNavClick()
+{
+    var items = $('#navbar-items');
+    var $body = $('body');
+    
+    items.on('shown.bs.collapse', function() {
+        $body.removeClass('padding-top-70px');
+        $body.addClass('padding-top-315px');
+    });
+    
+    items.on('hidden.bs.collapse', function() 
+    {
+        $body.removeClass('padding-top-315px');
+        $body.addClass('padding-top-70px');
+    });
+}
