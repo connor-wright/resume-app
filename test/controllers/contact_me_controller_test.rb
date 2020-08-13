60b8dd5b30a9228fa796ec3707a9cbfc7e75546a
Connor Wright <connor.d.wright1996@gmail.com>
2020-08-13 03:39:21 +0000

    Removed unneaded test... for now

--- test/controllers/contact_me_controller_test.rb
+++ test/controllers/contact_me_controller_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class ContactMeControllerTest < ActionDispatch::IntegrationTest
-  test "should get send" do
-    get contact_me_send_url
-    assert_response :success
-  end
-
 end
